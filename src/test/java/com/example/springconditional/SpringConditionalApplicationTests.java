e6ba3f3082ee442e1172f0fb4c7199a4ede78b35
Sam1549 <Sam1549@yandex.ru>
2023-09-21 18:02:52 +0400

    add testContainers

--- src/test/java/com/example/springconditional/SpringConditionalApplicationTests.java
+++ src/test/java/com/example/springconditional/SpringConditionalApplicationTests.java
@@ -1,13 +1,51 @@
 package com.example.springconditional;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestComponent;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.ResponseEntity;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
-class SpringConditionalApplicationTests {
+@Testcontainers
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class DemoApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @Container
+    private static final GenericContainer<?> devapp = new GenericContainer<>("devapp:latest")
+            .withExposedPorts(8080);
+
+    @Container
+    private static final GenericContainer<?> prodapp = new GenericContainer<>("prodapp:latest")
+            .withExposedPorts(8081);
+
+    @BeforeAll
+    public static void setUp() {
+//        devapp.start();
+//        prodapp.start();
+    }
 
     @Test
-    void contextLoads() {
+    void devProdTest() {
+        String expected = "Current profile is dev";
+        ResponseEntity<String> devAppEntity = restTemplate.getForEntity("http://localhost:" + devapp.getMappedPort(8080) + "/profile", String.class);
+        System.out.println(devAppEntity.getBody());
+        Assertions.assertEquals(expected, devAppEntity.getBody());
+    }
+
+    @Test
+    void prodAppTest() {
+        String expected = "Current profile is production";
+        ResponseEntity<String> prodAppEntity = restTemplate.getForEntity("http://localhost:" + prodapp.getMappedPort(8081) + "/profile", String.class);
+        System.out.println(prodAppEntity.getBody());
+        Assertions.assertEquals(expected, prodAppEntity.getBody());
     }
 
 }
